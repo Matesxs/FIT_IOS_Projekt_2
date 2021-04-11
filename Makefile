cb32620b33f4d4d481f13e0fc9b8770b35ccda41
MatesXs <martindousa186@gmail.com>
2021-04-11 14:58:31 +0200

    added cleaning script

--- Makefile
+++ Makefile
@@ -9,6 +9,12 @@ build: $(CLIST)
 run: build
 	@./proj2 5 3 600 435
 
+.PHONY: cleand
+cleand:
+	chmod +x clean.sh
+	./clean.sh
+
+.PHONY: clean
 clean:
 	@-rm proj2
 	@-rm proj2.out
---
+++ clean.sh
@@ -0,0 +1,4 @@
+ipcs -tm | grep "$(whoami)" | awk '{print $1};' | xargs -L1 ipcrm -m
+ipcs -ts | grep "$(whoami)" | awk '{print $1};' | xargs -L1 ipcrm -s
+find /dev/shm -user "$(whoami)" -delete
+killall proj2
